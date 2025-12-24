d8a0b44fa13b2512fa9ea7a0bf7649f63dafa089
Joel <agboolajoel123@gmail.com>
2025-12-24 21:10:39 +0100

    feat: add URL model

---
+++ src/Model/URL.Model.ts
@@ -0,0 +1,30 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUrl extends Document {
+  orginalUrl: string,
+  shortCode: string,
+  clicks: number,
+  createdAt: Date,
+}
+
+const urlSchema = new Schema<IUrl>({
+  orginalUrl: {
+    type: String,
+    required: true,
+  },
+  shortCode: {
+    type: String, 
+    required: true,
+    unique: true,
+  },
+  clicks: {
+    type: Number, 
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+});
+
+export default mongoose.model<IUrl>('Url', urlSchema)
